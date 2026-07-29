e9fe8d0b28e415f5e7544d1085844b05cd926623
Artur Albuquerque <arturl_albuquerque@hotmail.com>
2026-07-29 13:38:46 -0300

    ok

--- script.js
+++ script.js
@@ -67,3 +67,27 @@ function initSmoothScroll() {
   });
 }
 initSmoothScroll();
+
+function initAnimationScroll () {
+  const sections = document.querySelectorAll(".js-scroll");
+  if(sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+    
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top.toFixed();
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+    
+    animaScroll()
+    
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+initAnimationScroll ()
