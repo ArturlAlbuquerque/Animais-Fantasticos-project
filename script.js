e00dac9ba549b21c5997cd036de6b5ff2929842d
Artur Albuquerque <arturl_albuquerque@hotmail.com>
2026-07-29 10:44:30 -0300

    ok

--- script.js
+++ script.js
@@ -41,3 +41,29 @@ function initAccordion() {
 }
 
 initAccordion();
+
+function initSmoothScroll() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const sectionId = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(sectionId);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    /* //Forma alternativa
+    const topo = section.offsetTop;
+    window.scrollTo({
+      top: topo,
+      behavior: "smooth",
+    }); */
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+initSmoothScroll();
