b2ef02939d9448e75484865599bae78edf4c54c4
Artur Albuquerque <arturl_albuquerque@hotmail.com>
2026-08-27 20:05:27 -0300

    ok

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@ import initSmoothScroll from "./modules/scroll-suave.js";
 import initAnimationScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./tabnav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initSmoothScroll();
 initAnimationScroll();
 initAccordion();
 initTabNav();
+initModal();
+initTooltip();
